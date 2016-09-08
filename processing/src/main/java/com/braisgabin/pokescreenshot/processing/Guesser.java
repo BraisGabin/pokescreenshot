ecb5be1c070f12a1796034ac24660800c1cad84a
Brais Gabín <braisgabin@gmail.com>
2016-09-08 20:36:37 +0200

    Guess Pokémon

--- processing/src/main/java/com/braisgabin/pokescreenshot/processing/Guesser.java
+++ processing/src/main/java/com/braisgabin/pokescreenshot/processing/Guesser.java
@@ -1,5 +1,8 @@
 package com.braisgabin.pokescreenshot.processing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.braisgabin.pokescreenshot.processing.CP.CPM;
 import static java.lang.Math.floor;
 import static java.lang.Math.max;
@@ -14,5 +17,17 @@ public class Guesser {
     float CPM = CPM(lvl);
     // Formula extracted from: https://pokemongo.gamepress.gg/pokemon-stats-advanced
     return max(10, (int) round(floor(((atk) * sqrt(def) * sqrt(stam) * CPM * CPM) / 10)));
+  }
+
+  static Pokemon[] getPokemon(Pokemon[] pokemonList, int cp, float lvl) {
+    List<Pokemon> pokemon = new ArrayList<>();
+    for (Pokemon p : pokemonList) {
+      final int minCp = calculateCp(p, lvl, 0, 0, 0);
+      final int maxCp = calculateCp(p, lvl, 15, 15, 15);
+      if (minCp <= cp && maxCp >= cp) {
+        pokemon.add(p);
+      }
+    }
+    return pokemon.toArray(new Pokemon[pokemon.size()]);
   }
 }
--- processing/src/test/java/com/braisgabin/pokescreenshot/processing/GuesserUnitTest.java
+++ processing/src/test/java/com/braisgabin/pokescreenshot/processing/GuesserUnitTest.java
@@ -18,4 +18,59 @@ public class GuesserUnitTest {
     Pokemon pokemon = Pokemon.create(1, "", 100, 100, 100, "");
     assertThat(Guesser.calculateCp(pokemon, 1, 0, 0, 0), is(10));
   }
+
+  @Test
+  public void testGetPokemon_eevee() {
+    Pokemon[] pokemon = {
+        Pokemon.create(133, "Eevee", 114, 128, 110, "EEVEE"),
+        Pokemon.create(134, "Vaporeon", 186, 168, 260, "EEVEE"),
+        Pokemon.create(135, "Jolteon", 192, 174, 130, "EEVEE"),
+        Pokemon.create(136, "Flareon", 238, 178, 130, "EEVEE"),
+    };
+    assertThat(Guesser.getPokemon(pokemon, 482, 17), is(new Pokemon[]{pokemon[0]}));
+  }
+
+  @Test
+  public void testGetPokemon_vaporeon() {
+    Pokemon[] pokemon = {
+        Pokemon.create(133, "Eevee", 114, 128, 110, "EEVEE"),
+        Pokemon.create(134, "Vaporeon", 186, 168, 260, "EEVEE"),
+        Pokemon.create(135, "Jolteon", 192, 174, 130, "EEVEE"),
+        Pokemon.create(136, "Flareon", 238, 178, 130, "EEVEE"),
+    };
+    assertThat(Guesser.getPokemon(pokemon, 1173, 15), is(new Pokemon[]{pokemon[1]}));
+  }
+
+  @Test
+  public void testGetPokemon_jolteon() {
+    Pokemon[] pokemon = {
+        Pokemon.create(133, "Eevee", 114, 128, 110, "EEVEE"),
+        Pokemon.create(134, "Vaporeon", 186, 168, 260, "EEVEE"),
+        Pokemon.create(135, "Jolteon", 192, 174, 130, "EEVEE"),
+        Pokemon.create(136, "Flareon", 238, 178, 130, "EEVEE"),
+    };
+    assertThat(Guesser.getPokemon(pokemon, 736, 13), is(new Pokemon[]{pokemon[2]}));
+  }
+
+  @Test
+  public void testGetPokemon_flareon() {
+    Pokemon[] pokemon = {
+        Pokemon.create(133, "Eevee", 114, 128, 110, "EEVEE"),
+        Pokemon.create(134, "Vaporeon", 186, 168, 260, "EEVEE"),
+        Pokemon.create(135, "Jolteon", 192, 174, 130, "EEVEE"),
+        Pokemon.create(136, "Flareon", 238, 178, 130, "EEVEE"),
+    };
+    assertThat(Guesser.getPokemon(pokemon, 1415, 19), is(new Pokemon[]{pokemon[1], pokemon[3]}));
+  }
+
+  @Test
+  public void testGetPokemon_none() {
+    Pokemon[] pokemon = {
+        Pokemon.create(133, "Eevee", 114, 128, 110, "EEVEE"),
+        Pokemon.create(134, "Vaporeon", 186, 168, 260, "EEVEE"),
+        Pokemon.create(135, "Jolteon", 192, 174, 130, "EEVEE"),
+        Pokemon.create(136, "Flareon", 238, 178, 130, "EEVEE"),
+    };
+    assertThat(Guesser.getPokemon(pokemon, 50, 19), is(new Pokemon[]{}));
+  }
 }

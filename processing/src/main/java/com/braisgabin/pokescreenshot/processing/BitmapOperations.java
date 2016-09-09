07a032508c87101b53a54d86664a72a860d46d13
Brais Gabín <braisgabin@gmail.com>
2016-09-09 22:32:35 +0200

    Don't use support render script

--- processing/src/main/java/com/braisgabin/pokescreenshot/processing/BitmapOperations.java
+++ processing/src/main/java/com/braisgabin/pokescreenshot/processing/BitmapOperations.java
@@ -2,8 +2,8 @@ package com.braisgabin.pokescreenshot.processing;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.support.v8.renderscript.Allocation;
-import android.support.v8.renderscript.RenderScript;
+import android.renderscript.Allocation;
+import android.renderscript.RenderScript;
 
 class BitmapOperations {
 
@@ -11,6 +11,7 @@ class BitmapOperations {
     RenderScript rs = RenderScript.create(context);
     ScriptC_ocrpreprocess script = new ScriptC_ocrpreprocess(rs);
     Allocation allocation = Allocation.createFromBitmap(rs, bitmap);
+    script.set_script(script);
     script.invoke_process(allocation, value);
     allocation.copyTo(bitmap);
   }
--- processing/src/main/rs/ocrpreprocess.rs
+++ processing/src/main/rs/ocrpreprocess.rs
@@ -2,22 +2,22 @@
 #pragma rs java_package_name(com.braisgabin.pokescreenshot.processing)
 
 int VALUE;
+rs_script script;
 
-uchar4 RS_KERNEL ocrpreprocess(uchar4 in, uint32_t x, uint32_t y) {
-  uchar4 out = in;
-  uint32_t g = (uint32_t) ((in.r + in.g + in.b) / 3.);
+void root(const uchar4* in, uchar4* out, uint32_t x, uint32_t y) {
+  *out = *in;
+  uint32_t g = (uint32_t) ((in->r + in->g + in->b) / 3.);
   if (g <= VALUE) {
     g = 0;
   } else {
     g = 255;
   }
-  out.r = g;
-  out.g = g;
-  out.b = g;
-  return out;
+  out->r = g;
+  out->g = g;
+  out->b = g;
 }
 
 void process(rs_allocation image, int value) {
   VALUE = value;
-  rsForEach(ocrpreprocess, image, image);
+  rsForEach(script, image, image);
 }

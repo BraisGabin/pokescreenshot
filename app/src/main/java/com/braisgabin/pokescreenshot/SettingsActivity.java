ed545cfc012a0beece1ae4fad055f0c0d9043575
Brais Gabín <braisgabin@gmail.com>
2016-09-10 18:47:39 +0200

    Set lvl

---
+++ app/src/main/java/com/braisgabin/pokescreenshot/SettingsActivity.java
@@ -0,0 +1,99 @@
+package com.braisgabin.pokescreenshot;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+  public static Intent getCallingIntent(Context context) {
+    final Intent intent = new Intent(context, SettingsActivity.class);
+
+    return intent;
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_settings);
+    if (savedInstanceState == null) {
+      getFragmentManager()
+          .beginTransaction()
+          .add(R.id.f_for_fragment, SettingsFragment.newInstance())
+          .commit();
+    }
+  }
+
+
+  public static class SettingsFragment extends PreferenceFragment {
+    public static SettingsFragment newInstance() {
+      Bundle args = new Bundle();
+
+      SettingsFragment fragment = new SettingsFragment();
+      fragment.setArguments(args);
+      return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      addPreferencesFromResource(R.xml.settings);
+
+      bindPreferenceSummaryToValue(findPreference("trainer_lvl"));
+    }
+  }
+
+  /**
+   * Binds a preference's summary to its value. More specifically, when the
+   * preference's value is changed, its summary (line of text below the
+   * preference title) is updated to reflect the value. The summary is also
+   * immediately updated upon calling this method. The exact display format is
+   * dependent on the type of preference.
+   *
+   * @see #sBindPreferenceSummaryToValueListener
+   */
+  private static void bindPreferenceSummaryToValue(Preference preference) {
+    // Set the listener to watch for value changes.
+    preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+
+    // Trigger the listener immediately with the preference's
+    // current value.
+    sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
+        PreferenceManager
+            .getDefaultSharedPreferences(preference.getContext())
+            .getString(preference.getKey(), ""));
+  }
+
+  /**
+   * A preference value change listener that updates the preference's summary
+   * to reflect its new value.
+   */
+  private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+      String stringValue = value.toString();
+
+      if (preference instanceof ListPreference) {
+        // For list preferences, look up the correct display value in
+        // the preference's 'entries' list.
+        ListPreference listPreference = (ListPreference) preference;
+        int index = listPreference.findIndexOfValue(stringValue);
+
+        // Set the summary to reflect the new value.
+        preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+
+      } else {
+        // For all other preferences, set the summary to the value's
+        // simple string representation.
+        preference.setSummary(stringValue);
+      }
+      return true;
+    }
+  };
+}

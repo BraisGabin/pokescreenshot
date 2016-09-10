710c61c57b45ce5e653ed74849c02817cdda9a7c
Brais Gabín <braisgabin@gmail.com>
2016-09-10 13:39:37 +0200

    No activity and stoppable service

--- app/src/main/java/com/braisgabin/pokescreenshot/Utils.java
+++ app/src/main/java/com/braisgabin/pokescreenshot/Utils.java
@@ -1,7 +1,5 @@
 package com.braisgabin.pokescreenshot;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -11,17 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utils {
-
-  // Extracted from: http://stackoverflow.com/a/5921190/842697
-  public static boolean isMyServiceRunning(Context context, Class<?> serviceClass) {
-    ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-    for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-      if (serviceClass.getName().equals(service.service.getClassName())) {
-        return true;
-      }
-    }
-    return false;
-  }
 
   public static <T> List<T> list(SQLiteDatabase database, RowMapper<T> mapper, String sql, String... selectionArgs) {
     final Cursor cursor = database.rawQuery(sql, selectionArgs);

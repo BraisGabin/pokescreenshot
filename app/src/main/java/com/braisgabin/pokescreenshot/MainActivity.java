1d332536e1ca3c6889a1afaef46e7e0da1983d53
Brais Gabín <braisgabin@gmail.com>
2016-09-11 19:20:10 +0200

    Improve onboarding

--- app/src/main/java/com/braisgabin/pokescreenshot/MainActivity.java
+++ app/src/main/java/com/braisgabin/pokescreenshot/MainActivity.java
@@ -7,20 +7,43 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import com.f2prateek.rx.preferences.Preference;
+
+import javax.inject.Inject;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends AppCompatActivity {
-  private static final int PERMISSION_REQUEST_CODE = 0;
+  private static final int REQUEST_CODE_PERMISSION = 0;
+  private static final int REQUEST_CODE_TRAINER_LVL = 1;
+
+  @Inject
+  Preference<String> trainerLvl;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    App.component(this).inject(this);
+    checkTrainerLevel();
+  }
+
+  private void checkTrainerLevel() {
+    if (!trainerLvl.isSet()) {
+      startActivityForResult(SettingsActivity.getCallingIntent(this), REQUEST_CODE_TRAINER_LVL);
+      Toast.makeText(getApplicationContext(), getString(R.string.select_trainter_level), Toast.LENGTH_LONG).show();
+    } else {
+      runService();
+    }
+  }
+
+  private void runService() {
     final Intent intent = ScreenshotService.getCallingIntent(this);
     if (ContextCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE) != PERMISSION_GRANTED) {
-      ActivityCompat.requestPermissions(this, new String[]{READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+      ActivityCompat.requestPermissions(this, new String[]{READ_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION);
     } else {
       startService(intent);
       findViewById(R.id.progressBar).setVisibility(View.INVISIBLE);
@@ -29,9 +52,20 @@ public class MainActivity extends AppCompatActivity {
   }
 
   @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    switch (requestCode) {
+      case REQUEST_CODE_TRAINER_LVL:
+        checkTrainerLevel();
+        break;
+      default:
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+  }
+
+  @Override
   public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
     switch (requestCode) {
-      case PERMISSION_REQUEST_CODE:
+      case REQUEST_CODE_PERMISSION:
         if (grantResults[0] == PERMISSION_GRANTED) {
           startService(ScreenshotService.getCallingIntent(this));
         }

80a383ba567c3233eaa8c7ebf45e8004ab1c7ec7
Brais Gabín <braisgabin@gmail.com>
2016-09-12 20:49:06 +0200

    Change the arc formula

--- processing/src/test/java/com/braisgabin/pokescreenshot/processing/CPUnitTest.java
+++ processing/src/test/java/com/braisgabin/pokescreenshot/processing/CPUnitTest.java
@@ -31,12 +31,42 @@ public class CPUnitTest {
   }
 
   @Test
-  public void testRadian2Lvl() {
-    assertThat(radian2Lvl(22, Math.toRadians(270 - 103.2647f)), is(1.5f));
+  public void testRadian2Lvl_2_2() {
+    assertThat(radian2Lvl(2, Math.toRadians(180 - 80.6)), is(2f));
   }
 
   @Test
-  public void testLvl2Radian() {
-    assertThat(lvl2Radian(22, 1.5f), is(closeTo(Math.toRadians(270 - 103.2647f), 0.00001)));
+  public void testRadian2Lvl_2_2_5() {
+    assertThat(radian2Lvl(2, Math.toRadians(180 - 109.8)), is(2.5f));
+  }
+
+  @Test
+  public void testRadian2Lvl_2_3() {
+    assertThat(radian2Lvl(2, Math.toRadians(180 - 135.5)), is(3f));
+  }
+
+  @Test
+  public void testRadian2Lvl_22_1_5() {
+    assertThat(radian2Lvl(22, Math.toRadians(180 - 13.3)), is(1.5f));
+  }
+
+  @Test
+  public void testLvl2Radian_2_2() {
+    assertThat(lvl2Radian(2, 2f), is(closeTo(Math.toRadians(180 - 80.6), Math.toRadians(0.1))));
+  }
+
+  @Test
+  public void testLvl2Radian_2_2_5() {
+    assertThat(lvl2Radian(2, 2.5f), is(closeTo(Math.toRadians(180 - 109.8), Math.toRadians(0.1))));
+  }
+
+  @Test
+  public void testLvl2Radian_2_3() {
+    assertThat(lvl2Radian(2, 3f), is(closeTo(Math.toRadians(180 - 135.5), Math.toRadians(0.1))));
+  }
+
+  @Test
+  public void testLvl2Radian_22_1_5() {
+    assertThat(lvl2Radian(22, 1.5f), is(closeTo(Math.toRadians(180 - 13.3), Math.toRadians(0.1))));
   }
 }

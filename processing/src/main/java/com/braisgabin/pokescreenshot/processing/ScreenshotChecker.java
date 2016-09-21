f0f7346ecd79a3a5013f184e3694ade1becf54af
Brais Gabín <braisgabin@gmail.com>
2016-09-21 16:01:31 +0200

    Fix typo

--- processing/src/main/java/com/braisgabin/pokescreenshot/processing/ScreenshotChecker.java
+++ processing/src/main/java/com/braisgabin/pokescreenshot/processing/ScreenshotChecker.java
@@ -6,14 +6,14 @@ public class ScreenshotChecker {
   private static final int BACKGROUND_COLOR = 0xfffafafa;
   private static final int BUTTON_COLOR = 0xff1c8796;
 
-  public static boolean isPokemonGoScreenshot(Bitmap bitamp) {
-    final int width = bitamp.getWidth();
-    final int height = bitamp.getHeight();
+  public static boolean isPokemonGoScreenshot(Bitmap bitmap) {
+    final int width = bitmap.getWidth();
+    final int height = bitmap.getHeight();
     final float d = width / 480f;
     final int x = Math.round(d * 40);
     for (int i = 6; i < 9; i++) {
       final int y = i * height / 10;
-      if (bitamp.getPixel(x, y) != BACKGROUND_COLOR || bitamp.getPixel(width - x, y) != BACKGROUND_COLOR) {
+      if (bitmap.getPixel(x, y) != BACKGROUND_COLOR || bitmap.getPixel(width - x, y) != BACKGROUND_COLOR) {
         return false;
       }
     }

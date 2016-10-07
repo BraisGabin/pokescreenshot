60b74b1080702554da9c6fd7f47c4420ca6cd43e
Brais Gabín <braisgabin@gmail.com>
2016-10-07 17:46:47 +0200

    Improve tests

--- processing/src/test/java/com/braisgabin/pokescreenshot/processing/OcrUnitTest.java
+++ processing/src/test/java/com/braisgabin/pokescreenshot/processing/OcrUnitTest.java
@@ -16,102 +16,55 @@ import static org.mockito.Mockito.when;
 
 public class OcrUnitTest {
   private Tess tess;
+  private Ocr ocr;
 
   @Before
-  public void setUp() {
+  public void setUp() throws Exception {
     this.tess = mock(Tess.class);
+    when(tess.cp(any(Rect.class))).thenReturn(100);
+    when(tess.name(anyInt())).thenReturn("Pika");
+    when(tess.hp(anyInt())).thenReturn(50);
+    when(tess.candy(anyInt())).thenReturn("PIKACHU");
+    when(tess.stardust(anyInt())).thenReturn(200);
+    
+    this.ocr = new Ocr(tess);
   }
 
   @Test
   public void testCp() throws Exception {
-    when(tess.cp(any(Rect.class))).thenReturn(100);
-
-    final Ocr ocr = new Ocr(tess);
     assertThat(ocr.cp(), is(100));
-  }
-
-  @Test
-  public void testCp_twice() throws Exception {
-    when(tess.cp(any(Rect.class))).thenReturn(100);
-
-    final Ocr ocr = new Ocr(tess);
-    ocr.cp();
-    ocr.cp();
+    assertThat(ocr.cp(), is(100));
     verify(tess, times(1)).cp(any(Rect.class));
   }
 
   @Test
   public void testName() throws Exception {
-    when(tess.name(anyInt())).thenReturn("Pika");
-
-    final Ocr ocr = new Ocr(tess);
     assertThat(ocr.name(), is("Pika"));
-  }
-
-  @Test
-  public void testName_twice() throws Exception {
-    when(tess.name(anyInt())).thenReturn("Pika");
-
-    final Ocr ocr = new Ocr(tess);
-    ocr.name();
-    ocr.name();
+    assertThat(ocr.name(), is("Pika"));
     verify(tess, times(1)).name(anyInt());
     verify(tess, times(1)).cp(any(Rect.class));
   }
 
   @Test
   public void testHp() throws Exception {
-    when(tess.hp(anyInt())).thenReturn(100);
-
-    final Ocr ocr = new Ocr(tess);
-    assertThat(ocr.hp(), is(100));
-  }
-
-  @Test
-  public void testHp_twice() throws Exception {
-    when(tess.hp(anyInt())).thenReturn(100);
-
-    final Ocr ocr = new Ocr(tess);
-    ocr.hp();
-    ocr.hp();
+    assertThat(ocr.hp(), is(50));
+    assertThat(ocr.hp(), is(50));
     verify(tess, times(1)).hp(anyInt());
     verify(tess, times(1)).cp(any(Rect.class));
   }
 
   @Test
   public void testCandy() throws Exception {
-    when(tess.candy(anyInt())).thenReturn("Pika");
-
-    final Ocr ocr = new Ocr(tess);
-    assertThat(ocr.candy(), is("Pika"));
-  }
-
-  @Test
-  public void testCandy_twice() throws Exception {
-    when(tess.candy(anyInt())).thenReturn("Pika");
-
-    final Ocr ocr = new Ocr(tess);
-    ocr.candy();
-    ocr.candy();
+    assertThat(ocr.candy(), is("PIKACHU"));
+    assertThat(ocr.candy(), is("PIKACHU"));
     verify(tess, times(1)).candy(anyInt());
     verify(tess, times(1)).cp(any(Rect.class));
   }
 
   @Test
   public void testStardust() throws Exception {
-    when(tess.stardust(anyInt())).thenReturn(100);
-
-    final Ocr ocr = new Ocr(tess);
-    assertThat(ocr.stardust(), is(100));
-  }
-
-  @Test
-  public void testStardust_twice() throws Exception {
-    when(tess.stardust(anyInt())).thenReturn(100);
-
-    final Ocr ocr = new Ocr(tess);
-    ocr.stardust();
-    ocr.stardust();
+    assertThat(ocr.stardust(), is(200));
+    assertThat(ocr.stardust(), is(200));
     verify(tess, times(1)).stardust(anyInt());
     verify(tess, times(1)).cp(any(Rect.class));
   }

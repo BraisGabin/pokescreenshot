bbf3808d853991c532c255d10aece1692d348fec
Brais Gabín <braisgabin@gmail.com>
2016-11-23 21:49:01 +0100

    Update Pokémon stats

--- app/src/main/java/com/braisgabin/pokescreenshot/model/MySQLiteOpenHelper.java
+++ app/src/main/java/com/braisgabin/pokescreenshot/model/MySQLiteOpenHelper.java
@@ -7,7 +7,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 public class MySQLiteOpenHelper extends SQLiteAssetHelper {
 
   private static final String DATABASE_NAME = "pokemon.db";
-  private static final int DATABASE_VERSION = 3;
+  private static final int DATABASE_VERSION = 4;
 
   public MySQLiteOpenHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
